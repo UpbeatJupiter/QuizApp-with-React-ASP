7e546985e6070b6866265640f71b3d0d4e175791
UpbeatJupiter <oeliftuncer@gmail.com>
2023-09-13 12:00:08 +0300

    Models Added

---
+++ webapi/Models/QuizDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Models
+{
+	public class QuizDBContext:DbContext
+	{
+		public QuizDBContext(DbContextOptions<QuizDBContext> options) : base(options) { }
+
+		public DbSet<Question> Questions { get; set; }
+		public DbSet<Participant> Participants { get; set; }
+	}
+}
