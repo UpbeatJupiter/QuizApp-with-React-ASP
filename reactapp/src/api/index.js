4a5fe6fa38f5bb3d731958e6e8339d714b2bfbdc
UpbeatJupiter <oeliftuncer@gmail.com>
2023-09-18 23:35:33 +0300

    update

--- reactapp/src/api/index.js
+++ reactapp/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://localhost:7110/';
+export const BASE_URL = 'http://localhost:5041/';
 
 export const ENDPOINTS = {
     participant: 'participant',
